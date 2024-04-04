e945c156299acfadade8f954c799803910f4cead
sanchi kataria <sanchikataria10@gmail.com>
2024-04-04 15:42:39 +0530

    commit

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const PORT=5555;
+export const PORT=5555 || process.env.PORT;
 export const mongoDBURL='mongodb+srv://skat261003:skat261003@mern.h80nift.mongodb.net/details-collection?retryWrites=true&w=majority&appName=mern';
